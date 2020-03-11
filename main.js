e9ae2f8d88b1f22c33b9134dde10092c9e9405d5
Calo Corp <marc.calo@codeimmersives.com>
2020-03-11 07:48:14 -0400

    work in progress

---
+++ main.js
@@ -0,0 +1,11 @@
+const mess = document.querySelector('.mess-with-me')
+
+mess.style.fontSize ='3em'
+
+
+
+mess.style.backgroundColor='green'
+
+const hideMe= document.querySelector('.hide-me-area')
+
+hideMe.hidden= true
