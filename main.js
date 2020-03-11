4da5b9a7c28150da7b5210017cfc3986ad7e72b6
Calo Corp <marc.calo@codeimmersives.com>
2020-03-11 11:39:18 -0400

    almost finished

--- main.js
+++ main.js
@@ -6,6 +6,35 @@ mess.style.fontSize ='3em'
 
 mess.style.backgroundColor='green'
 
-const hideMe= document.querySelector('.hide-me-area')
+const hideMe= document.querySelector('#hide-me')
 
 hideMe.hidden= true
+
+const editBluePowerRangerZoid =document.querySelector('#triceratops')
+
+editBluePowerRangerZoid.style.width=('324px')
+
+const addborder =function(){
+
+editBluePowerRangerZoid.style.border='5px solid red'
+} 
+
+editBluePowerRangerZoid.addEventListener('click', addborder)
+
+const editRowOfDinos=document.querySelector('#row')
+
+const changeRowBackgroundColor=function(){
+
+    editRowOfDinos.style.backgroundColor='purple'
+}
+editRowOfDinos.addEventListener('click',changeRowBackgroundColor)
+
+const editFeather=document.querySelector('#feathers')
+
+const changeTransparanceyFeathers=function(){
+    editFeather.style.opacity='0.5'
+
+}
+
+editFeather.addEventListener('click',changeTransparanceyFeathers)
+
